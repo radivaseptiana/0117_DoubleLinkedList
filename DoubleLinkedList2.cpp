3e099b15cd7afad52c520eb56364a48a7210242a
radivaseptiana <radivaseptiana51@gmail.com>
2025-05-15 16:12:11 +0700

    Menghapus node

--- DoubleLinkedList2.cpp
+++ DoubleLinkedList2.cpp
@@ -117,5 +117,18 @@ class DoubleLinkedList
                 START->prev = NULL; // Step 2b: START.prev = NULL
             }
         }
+        else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // Step 4: if current is not the last node
+            if (current->next != NULL)
+            current->next->prev = current->prev;
+        }
+
+        // Step 5: Delete the node
+        delete current;
+        cout << "record with roll number" <<rollNo << "deleted" << endl;
     }
 };
